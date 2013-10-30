99e7c74b9fbae7de6c6d0bb93154882a3cb98cba
’Alejandro <alexjabf@hotmail.com>
2013-10-29 18:21:12 -0700

    Cambios

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -3,9 +3,8 @@ class Sponsor < ActiveRecord::Base
   has_attached_file :logo,
     :styles => {:medium => "300x300>",
     :thumb => "100x100>",
-    :mobile => "32x32>" },
-    :default_url => "/assets/missing.jpg",
-    :path        => "pictures/:id/:filename"
+    :mobile => "32x32>"},
+    :default_url => "/assets/missing.jpg"
   
   validates :name, :phone, :email, :social_reason, :job, :presence => true
   validates :name, :uniqueness => true
