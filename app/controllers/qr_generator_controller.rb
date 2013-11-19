e21d4f9817d3a98054d1725e8ff73358f0456196
’Alejandro <alexjabf@hotmail.com>
2013-11-19 14:26:21 -0700

    Cambios

--- app/controllers/qr_generator_controller.rb
+++ app/controllers/qr_generator_controller.rb
@@ -1,6 +1,9 @@
 class QrGeneratorController < ApplicationController
   
   def generate_qr
+    @workshop_name = params[:workshop_name]
+    @teacher_name = params[:teacher_name]
+    @room = params[:room]
     @qr_code = "https://chart.googleapis.com/chart?cht=qr&chs=#{256}x#{256}&chl=#{SystemConfiguration.first.token}:#{params[:key]}"
     render layout: false
   end
--- db/schema.rb
+++ db/schema.rb
@@ -77,8 +77,6 @@ ActiveRecord::Schema.define(:version => 20131112202019) do
     t.datetime "updated_at",                          :null => false
   end
 
-  add_index "attendees", ["subgroup_id"], :name => "attendees_subgroup_id_fk"
-
   create_table "conferences", :force => true do |t|
     t.string   "name",         :null => false
     t.string   "conferencist", :null => false
@@ -138,8 +136,6 @@ ActiveRecord::Schema.define(:version => 20131112202019) do
     t.datetime "updated_at",  :null => false
   end
 
-  add_index "face_to_faces", ["attendee_id"], :name => "face_to_faces_attendee_id_fk"
-
   create_table "groups", :force => true do |t|
     t.string   "name",       :null => false
     t.datetime "created_at", :null => false
@@ -171,8 +167,6 @@ ActiveRecord::Schema.define(:version => 20131112202019) do
     t.datetime "updated_at",  :null => false
   end
 
-  add_index "nips", ["attendee_id"], :name => "nips_attendee_id_fk"
-
   create_table "offerts", :force => true do |t|
     t.integer  "exhibitor_id",                               :null => false
     t.string   "description",                                :null => false
@@ -183,8 +177,6 @@ ActiveRecord::Schema.define(:version => 20131112202019) do
     t.datetime "created_at",                                 :null => false
     t.datetime "updated_at",                                 :null => false
   end
-
-  add_index "offerts", ["exhibitor_id"], :name => "offerts_exhibitor_id_fk"
 
   create_table "ratings", :force => true do |t|
     t.integer  "value",      :null => false
@@ -214,10 +206,6 @@ ActiveRecord::Schema.define(:version => 20131112202019) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
-
-  add_index "schedules", ["hour_id"], :name => "schedules_hour_id_fk"
-  add_index "schedules", ["subgroup_id"], :name => "schedules_subgroup_id_fk"
-  add_index "schedules", ["workshop_id"], :name => "schedules_workshop_id_fk"
 
   create_table "sessions", :force => true do |t|
     t.string   "session_id", :null => false
@@ -264,8 +252,6 @@ ActiveRecord::Schema.define(:version => 20131112202019) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
   end
-
-  add_index "subgroups", ["group_id"], :name => "subgroups_group_id_fk"
 
   create_table "system_configurations", :force => true do |t|
     t.string   "token"
@@ -320,7 +306,6 @@ ActiveRecord::Schema.define(:version => 20131112202019) do
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-  add_index "users", ["role_id"], :name => "users_role_id_fk"
   add_index "users", ["unlock_token"], :name => "index_users_on_unlock_token", :unique => true
 
   create_table "workshops", :force => true do |t|
@@ -331,8 +316,6 @@ ActiveRecord::Schema.define(:version => 20131112202019) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
   end
-
-  add_index "workshops", ["room_id"], :name => "workshops_room_id_fk"
 
   add_foreign_key "attendees", "subgroups", :name => "attendees_subgroup_id_fk"
 
