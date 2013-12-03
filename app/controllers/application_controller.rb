0b9a762548f7ea63d57793bb97e9a1447cf3e5ec
Joel Melendez <jmelendez@emobile.com.mx>
2013-12-03 14:55:07 -0700

    Se modificó el controlador de la aplicación para manejar los idiomas.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :set_locale
+  #before_filter :set_locale
   layout :layout_by_resource
 
   def layout_by_resource
@@ -12,10 +12,8 @@ class ApplicationController < ActionController::Base
   end
   
   def set_locale
-    if session[:attendee_id].nil?
-      I18n.locale = params[:locale] || I18n.default_locale
-      #I18n.locale = "en"
-    end
+    I18n.locale = params[:locale] || I18n.default_locale
+    #I18n.locale = "en"
   end
   
 end
